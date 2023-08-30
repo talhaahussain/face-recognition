1153a373791ed86b0658b4a71862d22fa292dafc
talhaahussain <talhaahussain@gmail.com>
2023-08-30 19:06:47 +0100

    Added video capture initialisation
    Untested - may or may not work on host platform

--- data-collection.py
+++ data-collection.py
@@ -2,3 +2,12 @@ import os
 import time
 import uuid
 import cv2
+
+IMAGES_PATH = os.path.join('data', 'images')
+number_images = 30
+
+cap = cv2.VideoCapture(1)
+for imgnum in range(number_images):
+    print('Collecting image {}'.format(imgnum))
+    ret, frame = cap.read()
+    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
