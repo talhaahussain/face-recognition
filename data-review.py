820d44d7734d7b0f55ca8e4c3bc484987e9b6d8f
talhaahussain <talhaahussain@gmail.com>
2023-08-31 19:52:28 +0100

    Added TensorFlow limitations to GPU usage
    data-review.py is wholly untested
    labelme continues not to work, leading to two options:
    a) Use alternative software for image labelling
    b) Use secondary (pre-labelled) images of faces
    Decision pending.

--- data-review.py
+++ data-review.py
@@ -3,3 +3,9 @@ import json
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+tf.config.list_physical_devices('GPU')
